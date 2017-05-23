07e7360901ecaa5e8197f6214cc17124ce948ac3
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2017-05-23 08:51:04 +0200

    Exercice 1 Suppression Test Exercice 14

--- src/main/java/fr/univ_amu/iut/exercice2/MySecondJavaFXWindow.java
+++ src/main/java/fr/univ_amu/iut/exercice2/MySecondJavaFXWindow.java
@@ -11,6 +11,11 @@ public class MySecondJavaFXWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Second Useless JavaFX Window");
+        primaryStage.setAlwaysOnTop(true);
+        primaryStage.setResizable(false);
+        primaryStage.setWidth(800);
+        primaryStage.setHeight(400);
+        primaryStage.show();
     }
 }
--- src/test/java/fr/univ_amu/iut/exercice1/TestMyFirstJavaFXWindow.java
+++ src/test/java/fr/univ_amu/iut/exercice1/TestMyFirstJavaFXWindow.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.fail;
 
 public class TestMyFirstJavaFXWindow extends ApplicationTest {
 
-    @Ignore
+    //@Ignore
     @Test
     public void should_initialize_stage_with_correct_title() {
         try {
