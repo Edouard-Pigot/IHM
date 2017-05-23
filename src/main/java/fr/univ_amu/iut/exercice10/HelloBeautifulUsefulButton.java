5188e5086260784c02ea97fd84a2ce6c40641856
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2017-05-23 11:51:29 +0200

    Exercice 10

--- src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
+++ src/main/java/fr/univ_amu/iut/exercice10/HelloBeautifulUsefulButton.java
@@ -1,6 +1,14 @@
 package fr.univ_amu.iut.exercice10;
 
+import fr.univ_amu.iut.exercice9.EcouteurSimple;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class HelloBeautifulUsefulButton extends Application {
@@ -8,6 +16,30 @@ public class HelloBeautifulUsefulButton extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        primaryStage.setTitle("Hello !");
+        primaryStage.setWidth(250);
+        primaryStage.setHeight(100);
+        BorderPane borderPane = new BorderPane();
+        Image image = new Image("https://raw.githubusercontent.com/IUTInfoAix-M2105/Syllabus/master/assets/logo.png");
+        ImageView imageView = new ImageView();
+        imageView.setImage(image);
+        Button button = new Button("Hello !", imageView);
+        button.setId("buttonHello");
+        EcouteurSimple ecouteurSimple = new EcouteurSimple();
+        EventHandler<ActionEvent> ecouteur = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                ++nbAction;
+                System.out.println("Bouton actionné " + nbAction + " fois");
+            }
+        };
+        button.setOnAction(ecouteur);
+        //button.setText("Hello !");
+        Scene scene = new Scene(borderPane);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("DarkTheme.css").toExternalForm());
+        primaryStage.setHeight(250);
+        primaryStage.setScene(scene);
+        borderPane.setCenter(button);
+        primaryStage.show();
     }
 }
